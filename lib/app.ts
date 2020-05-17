7cfd370474dddd3d3ab12a447c2e143735c44a70
savalone47 <savalonescott47@gmail.com>
2020-05-17 13:46:47 +0200

    main principal update to database

--- lib/app.ts
+++ lib/app.ts
@@ -26,7 +26,7 @@ class App {
 	}
 	private mongoSetup():void {
 		mongoose.Promise = global.Promise;
-		mongoose.connect(this.mongoUrl);
+		mongoose.connect(this.mongoUrl,{ useUnifiedTopology: true });
 	}
 }
 export default new App().app;

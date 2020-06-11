eb69a0e4ea9a83c73ac5de6e1e8e34ee993c8187
savalone47 <savalonescott47@gmail.com>
2020-06-11 19:13:40 +0200

    update app.js

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import * as bodyParser from "body-parser";
+
 import * as express from "express";
+
 import * as mongoose from "mongoose";
+
 import { Routes } from "./routes/crmRoutes";
 
 class App {
@@ -18,14 +21,13 @@ class App {
 	private config():void {
 		this.app.use(bodyParser.json());
 		this.app.use(bodyParser.urlencoded(
-			{extended: false}
+			{extended: true}
 		));
 	}
 	private mongoSetup():void {
 		mongoose.Promise = global.Promise;
-		mongoose.connect(
-			this.mongoUrl,
-			{ useUnifiedTopology: true }
+		mongoose.connect(this.mongoUrl,
+			{ useNewUrlParser: true },
 		);
 	}
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 // import the main principal page
-import app from "./app"; 
+import app from "./app";
 
-// import the port from envorinement file .env
-const PORT = process.env.PORT;  
+const PORT = 3000;
 
 // Running the Server
 app.listen(PORT, () => console.log(`listing on localhost:${PORT}`));
